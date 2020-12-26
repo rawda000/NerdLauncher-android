ebc9d4386113da19503c7b81c44fbd1c172fc49d
Rawda Yasser <rawdayasser2019@gmail.com>
2020-12-27 00:33:23 +0200

    Added as home screen option

--- app/src/main/java/com/rawda/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/rawda/nerdlauncher/NerdLauncherFragment.java
@@ -87,7 +87,7 @@ public class NerdLauncherFragment extends Fragment {
             ActivityInfo activityInfo = mResolveInfo.activityInfo;
             Intent i = new Intent(Intent.ACTION_MAIN)
                     .setClassName(activityInfo.applicationInfo.packageName,
-                            activityInfo.name);
+                            activityInfo.name).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(i);
         }
     }
